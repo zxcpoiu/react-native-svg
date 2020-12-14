7d84001f3874a0f16881618d63f6ac040c3e5aad
zxcpoiu <zxcpoiu@gmail.com>
2020-12-15 01:08:36 +0800

    ios: rename `distance`

--- ios/Utils/RNSVGPathMeasure.m
+++ ios/Utils/RNSVGPathMeasure.m
@@ -18,9 +18,9 @@
 static CGFloat idealFlatness = (CGFloat).01;
 
 /**
- * returns the distance between two points
+ * returns the distanceBetween between two points
  */
-CGFloat distance(CGPoint p1, CGPoint p2)
+CGFloat distanceBetween(CGPoint p1, CGPoint p2)
 {
     CGFloat dx = p2.x - p1.x;
     CGFloat dy = p2.y - p1.y;
@@ -88,7 +88,7 @@ void subdivideBezierAtT(const CGPoint bez[4], CGPoint bez1[4], CGPoint bez2[4], 
 
 - (void)addLine:(CGPoint *)last next:(const CGPoint *)next {
     NSArray *line = @[[NSValue valueWithCGPoint:*last], [NSValue valueWithCGPoint:*next]];
-    _pathLength += distance(*last, *next);
+    _pathLength += distanceBetween(*last, *next);
     [_lengths addObject:[NSNumber numberWithDouble:_pathLength]];
     [_lines addObject:line];
     *last = *next;
@@ -153,10 +153,10 @@ void subdivideBezierAtT(const CGPoint bez[4], CGPoint bez1[4], CGPoint bez2[4], 
                     CGPoint ctrl2 = bez[2];
                     CGPoint next = bez[3];
                     CGFloat polyLen =
-                        distance(last, ctrl1) +
-                        distance(ctrl1, ctrl2) +
-                        distance(ctrl2, next);
-                    CGFloat chordLen = distance(last, next);
+                        distanceBetween(last, ctrl1) +
+                        distanceBetween(ctrl1, ctrl2) +
+                        distanceBetween(ctrl2, next);
+                    CGFloat chordLen = distanceBetween(last, next);
                     CGFloat error = polyLen - chordLen;
 
                     // if the error is less than our accepted level of error
